607ed195a76261f2a34bf88d4aa41c2c731d682b
Pranav Arora <pranav2vis@gmail.com>
2017-06-18 02:05:22 +0530

    Add files via upload

---
+++ reciever.sh
@@ -0,0 +1,60 @@
+
+
+
+if [ $# -ne 1 ]
+then
+echo "Wrong number of argurments. Exiting...."
+fi
+
+filepath=$1
+
+#  1 sender 
+#  2 receiver 
+
+
+./transport_layer 3
+r=$?
+if [ $r -ne 0 ]
+then
+echo "Error in Transport Layer!!"
+exit -1
+fi
+
+
+./mac_layer 2
+
+r=$?
+if [ $r -ne 0 ]
+then
+echo "Error in MAC Layer!!"
+exit -1
+fi
+
+
+./network_layer 2
+
+r=$?
+if [ $r -ne 0 ]
+then
+echo "Error in Network Layer!!"
+exit -1
+fi
+
+
+./transport_layer 2
+r=$?
+if [ $r -ne 0 ]
+then
+echo "Error in Transport Layer2!!"
+exit -1
+fi
+
+
+./application_layer 2 $filepath
+
+r=$?
+if [ $r -ne 0 ]
+then
+echo "Error in Application Layer!!"
+exit -1
+fi
